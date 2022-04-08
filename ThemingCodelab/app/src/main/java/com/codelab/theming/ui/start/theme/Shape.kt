9464f9cc1cc301d8d6138a274c0341838c8b4613
SH CHoi <csh153@gmail.com>
2022-04-08 18:54:27 +0900

    Shape 적용

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,17 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ *  Note
+ *
+ *  - CutCornerShape, RoundedCornerShape을 통해 shape 적용 가능
+ */
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -20,6 +20,7 @@ fun JetnewsTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = LightColors,
         typography = JetnewsTypography,
+        shapes = JetnewsShapes,
         content = content
     )
 }
