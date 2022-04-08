e42d219c0dfd79d99f3fd19adcd27a718f67f5c1
SH CHoi <csh153@gmail.com>
2022-04-08 18:49:30 +0900

    코드랩 절반 진행

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ *  Note
+ *
+ *  - 컬러명 지정시에는 primary등을 포함하지 않는 컬러의 고유 네이밍을 하도록 하는 것을 권장
+ *  - 이유는 각 정의한 테마들에 따라 각기 다른 primary 컬러 값을 가져갈 수 있기 때문
+ */
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
